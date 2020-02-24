724510cd973f6b56b0c4a4e8ffea48b19789fbe4
Larhrib Abdelaziz <56239817+abdelaziz199@users.noreply.github.com>
2020-02-24 13:41:19 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -1,22 +1,19 @@
-# on listons les nombres inferieurs a 10 , 
-# on prend 3 ou 5 ,on cherche les multiples de 3 ou bien 5   , dans le cas de 10   3= 3,6,9  . 5=5
-# on fait la somme = 3+6+9+5=23
+# mathematical problem
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
 
 
-# le probleme on cherche la somme des multiples de 3 ou 5 en dessous de 1000
-
-
-
-number_limit = int(input('entrer le nombre limite :'))
-multiple_trois = int(input('entrer le premier multiple :'))
-multiple_cinq = int(input('entrer le deuxieme multiple :'))
-somme =0
+number_limit = int(input('enter the limit:'))
+multiple_trois = int(input('enter the first multiple:'))
+multiple_cinq = int(input('enter second multiple :'))
+sum =0
 cpt =1 
 while cpt < number_limit:
     
-    if cpt%multiple_trois == 0 or cpt%multiple_cinq == 0:
-        somme += cpt
+    if cpt%multiple_three == 0 or cpt%multiple_five == 0:
+        sum += cpt
         
     cpt += 1
         
-print(somme)
+print(sum)
